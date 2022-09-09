3ca99b9a1824f20ddba6f1c3d5e972fdc8473137
JackiieM <jackie.nct@gmail.com>
2022-09-08 22:15:16 -0300

    datos de proyectos

---
+++ projects.js
@@ -0,0 +1,147 @@
+export default {
+    "projects": [
+        {
+            "name": "Le JacquEm",
+            "about": "Aplicación web que permite al usuario validar el número de una tarjeta de crédito para realizar una compra en la chocolatería Le JacquEm, en base al algoritmo de Luhn.",
+            "time": "Jun 29, 2022 a Jul 6, 2022",
+            "image": "https://raw.githubusercontent.com/JackiieM/SCL021-card-validation/main/src/assets/readme/final1.png",
+            "demo": "https://jackiiem.github.io/SCL021-card-validation/src/index.html",
+            "repo": "https://github.com/JackiieM/SCL021-card-validation",
+            "tools": [
+                {
+                    "tool": "Javascript",
+                    "icon": "https://api.iconify.design/mdi:language-javascript.svg?color=%23ffc800",
+                },
+                {
+                    "tool": "CSS",
+                    "icon": "https://api.iconify.design/akar-icons:css-fill.svg?color=%230084ff",
+                },
+                {
+                    "tool": "HTML5",
+                    "icon": "https://api.iconify.design/logos:html-5.svg?color=%230084ff",
+                },
+                {
+                    "tool": "Replit",
+                    "icon": "https://api.iconify.design/vscode-icons:file-type-light-replit.svg?color=%230084ff",
+                },
+                {
+                    "tool": "Git",
+                    "icon": "https://api.iconify.design/mdi:git.svg?color=%23ff7300",
+                },
+                {
+                    "tool": "Github",
+                    "icon": "https://api.iconify.design/mdi:github.svg?color=%23000000",
+                },
+                {
+                    "tool": "Figma",
+                    "icon": "https://api.iconify.design/logos:figma.svg?color=%23000000",
+                },
+                {
+                    "tool": "Trello",
+                    "icon": "https://api.iconify.design/logos:trello.svg?color=%23000000",
+                },
+                {
+                    "tool": "Adobe Photoshop",
+                    "icon": "https://api.iconify.design/vscode-icons:file-type-photoshop.svg?color=%23000000",
+                },
+            ] 
+        },
+        {
+            "name": "Ghiblipedia",
+            "about": "Aplicación web que permite al usuario visualizar, filtrar, y ordenar data con temática de las películas de Estudio Ghibli",
+            "time": "Jul 21, 2022 a Ago 8, 2022",
+            "image": "https://cdn.discordapp.com/attachments/704073351051870209/1017595135544008756/unknown.png",
+            "demo": "https://jackiiem.github.io/SCL021-data-lovers/src/index.html",
+            "repo": "https://github.com/JackiieM/SCL021-data-lovers",
+            "tools": [
+                {
+                    "tool": "Javascript",
+                    "icon": "https://api.iconify.design/mdi:language-javascript.svg?color=%23ffc800",
+                },
+                {
+                    "tool": "CSS",
+                    "icon": "https://api.iconify.design/akar-icons:css-fill.svg?color=%230084ff",
+                },
+                {
+                    "tool": "HTML5",
+                    "icon": "https://api.iconify.design/logos:html-5.svg?color=%230084ff",
+                },
+                {
+                    "tool": "Replit",
+                    "icon": "https://api.iconify.design/vscode-icons:file-type-light-replit.svg?color=%230084ff",
+                },
+                {
+                    "tool": "Git",
+                    "icon": "https://api.iconify.design/mdi:git.svg?color=%23ff7300",
+                },
+                {
+                    "tool": "Github",
+                    "icon": "https://api.iconify.design/mdi:github.svg?color=%23000000",
+                },
+                {
+                    "tool": "Figma",
+                    "icon": "https://api.iconify.design/logos:figma.svg?color=%23000000",
+                },
+                {
+                    "tool": "Trello",
+                    "icon": "https://api.iconify.design/logos:trello.svg?color=%23000000",
+                },
+                {
+                    "tool": "Adobe Photoshop",
+                    "icon": "https://api.iconify.design/vscode-icons:file-type-photoshop.svg?color=%23000000",
+                },
+            ] 
+        },
+        {
+            "name": "Niverse",
+            "about": "Red social para los amantes de los videojuegos de Nintendo. Permite registrarse con sus propios de datos o a través de una cuenta de google. Los registros se manejan en base a Firebase y los datos de perfil y posts se almacenan en Firestore.",
+            "time": "Ago 17, 2022 a",
+            "image": "https://cdn.discordapp.com/attachments/704073351051870209/1017595135544008756/unknown.png",
+            "demo": "https://jackiiem.github.io/SCL021-data-lovers/src/index.html",
+            "repo": "https://github.com/JackiieM/SCL021-social-network",
+            "tools": [
+                {
+                    "tool": "Javascript",
+                    "icon": "https://api.iconify.design/mdi:language-javascript.svg?color=%23ffc800",
+                },
+                {
+                    "tool": "CSS",
+                    "icon": "https://api.iconify.design/akar-icons:css-fill.svg?color=%230084ff",
+                },
+                {
+                    "tool": "HTML5",
+                    "icon": "https://api.iconify.design/logos:html-5.svg?color=%230084ff",
+                },
+                {
+                    "tool": "Visual Studio Code",
+                    "icon": "https://api.iconify.design/vscode-icons:file-type-light-replit.svg?color=%230084ff",
+                },
+                {
+                    "tool": "Firebase",
+                    "icon": "https://api.iconify.design/logos:firebase.svg?color=%23000000",
+                },
+                {
+                    "tool": "Git",
+                    "icon": "https://api.iconify.design/mdi:git.svg?color=%23ff7300",
+                },
+                {
+                    "tool": "Github",
+                    "icon": "https://api.iconify.design/mdi:github.svg?color=%23000000",
+                },
+                {
+                    "tool": "Figma",
+                    "icon": "https://api.iconify.design/logos:figma.svg?color=%23000000",
+                },
+                {
+                    "tool": "Trello",
+                    "icon": "https://api.iconify.design/logos:trello.svg?color=%23000000",
+                },
+                {
+                    "tool": "Adobe Photoshop",
+                    "icon": "https://api.iconify.design/vscode-icons:file-type-photoshop.svg?color=%23000000",
+                },
+            ] 
+        },
+
+    ]
+}

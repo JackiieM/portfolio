49794792722c623c45acb0781629d4d5e76b583c
JackiieM <jackie.nct@gmail.com>
2022-09-09 20:59:34 -0300

    tarjetas de proyecto dinámicas avance

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import data from "./projects.js"
 // Variables de data
+const allProjects = data.projects
+console.log(allProjects.length)
 const titles = data.projects.map(data => data.name);
 const descriptions = data.projects.map(data => data.about);
 const dates = data.projects.map(data => data.time);
@@ -7,4 +9,30 @@ const pictures = data.projects.map(data => data.image);
 const demoLinks = data.projects.map(data => data.demo);
 const repoLinks = data.projects.map(data=> data.repo);
 const toolsArr = data.projects.map(data => data.tools);
+let projectCards = [];
 
+window.addEventListener("load", createProjectCards);
+function createProjectCards() {
+    document.getElementById("mainDataCont").innerHTML = "";
+    for(let i = 0; i < allProjects.length; i++){
+        projectCards = `
+        <div id="projectCont">
+        <div id="titleCont">
+            <h1>${titles[i]}</h1>
+        </div>
+        <div id="projectBodyCont">
+            <div id="projectImg"><img  src="${pictures[i]}"></div>
+            <div id="projectInfoCont">
+                <p>${descriptions[i]}</p>
+                <div id="btnCont">
+                    <a class="linkBtn" href="${demoLinks[i]}"><button>Demo</button></a>
+                    <a class="linkBtn" href="${repoLinks[i]}"><button>Repo</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    document.getElementById("mainDataCont").innerHTML += projectCards;
+
+    }
+}
